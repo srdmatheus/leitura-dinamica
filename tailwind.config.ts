23d80c4d3c58a90fdd8951d444fede074f9f0202
Matheus Ribeiro <srdmatheus@gmail.com>
2023-11-27 13:50:38 -0300

    chore: add custom styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,12 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        brand: '#3fcf8e',
+        'brand-button': '#2b805a'
+      }
+    }
   },
   plugins: []
 };
