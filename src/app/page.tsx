bde841f5ee1ba4b67ee1d25ea90108ce82b6bc11
Matheus Ribeiro <srdmatheus@gmail.com>
2023-11-27 18:37:51 -0300

    wip: home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+'use client';
+
+import { Settings } from '@/components/settings';
+import { WordPasser } from '@/components/word-passer';
+import { usePlayerContext } from '@/contexts/player';
+import { useState } from 'react';
+
 export default function Home() {
-  return <h1>hello world</h1>;
+  const [editMode, setEditMode] = useState(true);
+  const [variantMode, setVariantMode] = useState<'unique' | 'text'>('unique');
+  const { updateText, text } = usePlayerContext();
+
+  return (
+    <div className="mx-auto flex max-w-[800px] flex-col gap-8">
+      {editMode ? (
+        <textarea
+          className="flex h-[280px] w-full resize-none rounded-md p-4 outline-none ring-brand focus:ring-2"
+          value={text}
+          onChange={(e) => updateText(e.target.value)}
+        />
+      ) : (
+        <WordPasser className="h-[280px]" variant={variantMode} />
+      )}
+      <Settings
+        editMode={editMode}
+        setEditMode={setEditMode}
+        setVariantMode={setVariantMode}
+      />
+    </div>
+  );
 }
