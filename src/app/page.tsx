5f44c7430ef105c008d82c1c54894c9945b2ff47
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-01 19:31:01 -0300

    refact: split code into components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,16 @@
 'use client';
 
-import { Settings } from '@/components/settings';
+import { Player } from '@/components/player';
 import { SideBar } from '@/components/side-bar';
-import { WordPasser } from '@/components/word-passer';
-import { usePlayerContext } from '@/contexts/player';
 import { useState } from 'react';
 
 export default function Home() {
-  const [editMode, setEditMode] = useState(true);
-  const [variantMode, setVariantMode] = useState<'unique' | 'text'>('unique');
   const [sideBarIsOpen, setSideBarIsOpen] = useState(false);
-  const { updateText, text } = usePlayerContext();
 
   return (
     <div className="mx-auto flex h-[100dvh] items-center justify-center">
-      <SideBar
-        className={sideBarIsOpen ? '-right-96' : ''}
-        onClick={() => setSideBarIsOpen((prev) => !prev)}
-      />
-      <div className="flex min-w-[800px] flex-col gap-8">
-        {editMode ? (
-          <textarea
-            className="flex h-[280px] w-full resize-none rounded-md p-4 outline-none ring-brand focus:ring-2"
-            value={text}
-            onChange={(e) => updateText(e.target.value)}
-          />
-        ) : (
-          <WordPasser className="h-[280px]" variant={variantMode} />
-        )}
-        <Settings
-          editMode={editMode}
-          setEditMode={setEditMode}
-          setVariantMode={setVariantMode}
-        />
-      </div>
+      <SideBar isOpen={sideBarIsOpen} handleToggleOpen={setSideBarIsOpen} />
+      <Player />
     </div>
   );
 }
